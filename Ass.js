006fd7dd01d4ed537288cd7c43818c236ea16205
s-c-n <samuel.nwafor975@gmail.com>
2024-08-05 02:43:57 -0700

    Add files via upload

---
+++ Ass.js
@@ -0,0 +1,260 @@
+import React from 'react'
+import "./Styles.css"
+import myImage from './photoss/logo.png';
+import mysImage from './photoss/slide.png' 
+import myImages from './photoss/orange.png' 
+import myImagess from './photoss/grapes.png' 
+import myImagel from './photoss/gauva.png' 
+import myPic from './photoss/fruit.jpeg' 
+import myPicture from './photoss/client.png' 
+function Ass() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul>
+        <div className='images'>
+      <img src={myImage} alt='bbb' width={60}/>
+    </div>
+            <ol className='wait'>Tropiko</ol>
+        </ul>
+        <ul>
+       <li><a className="navbar-brand" href="/">Home</a></li> 
+       <li><a className="navbar-brand" href="/">Fruit</a></li> 
+       <li><a className="navbar-brand" href="/">Service</a></li> 
+       <li><a className="navbar-brand" href="/">Contact Us</a></li> 
+        </ul>
+       <span className='was'><i class="fa-solid fa-magnifying-glass"></i></span>
+       <button className='me'>GET A QUOTE</button> 
+    </nav>
+    <div>
+    <div id="indicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="0" className="active"></button>
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="1"></button>
+    </div>  
+    <div class="container9">
+ <div class="bg-success navbar-success bg-opacity-10">
+  <div class="carousel-inner">
+  <div class="carousel-item active">
+  <h1 className='container7'>Welcome to <br></br> our Fruit Shop</h1>
+    <p className='pug'>There are many variations of passages of Lorem Ipsum available, <br></br> but the majority have suffered alteration in some form, by injected <br></br> humour, or randomised words which don't look even slightly <br></br> believable.</p>
+    <br></br>
+    <button>Shop Now</button>
+    <br></br>
+    <button className='but'>Contact US</button>
+    <div className='imag'>
+      <img src={mysImage} alt='bbb' width={600}/>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#indicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      </button>
+    
+      <button class="carousel-control-next" type="button" data-bs-target="#indicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon"></span>
+      </button>
+      </div>
+      </div> 
+      <div id="indicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+      <button type="button" data-bs-target="#indicators" data-bs-slide-to="0" class="active"></button>
+      <button type="button" data-bs-target="#indicators" data-bs-slide-to="1"></button> 
+    </div>
+    <div class="carousel-inner">
+    <div class="carousel-item">
+    <div class="container9">
+    <h1 className='container7'>Welcome to <br></br> our Fruit Shop</h1>
+      <p className='pug'>There are many variations of passages of Lorem Ipsum available, <br></br> but the majority have suffered alteration in some form, by injected <br></br> humour, or randomised words which don't look even slightly <br></br> believables.</p>
+      <br></br>
+      <button>Shop Now</button>
+      <br></br>
+      <button className='but'>Contact US</button>
+      <div className='imag'>
+        <img src={mysImage} alt='bbb' width={600}/>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#indicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </button>
+  
+      <button class="carousel-control-next" type="button" data-bs-target="#indicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </button>
+        </div>
+  </div> 
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <p className='wwe'>Our Services</p>
+    <p className='ww'>There are many variations of passages of Lorem Ipsum available, but the majority have</p>
+    <br></br>
+        <div className='ima'>
+      <img src={myImage} alt='bbb' width={60}/>
+    </div>
+    <div>
+      <table>
+        <tr>
+          <th> <span className='roi'> Orange</span> <br></br> <span className='qqq'>There are many variations of passages <br></br> of Lorem Ipsum available, but the <br></br> majority have suffered alteration in <br></br> some form, by injected humour, or <br></br> randomised words which don't look <br></br> even slightly believable. </span> </th>
+        </tr>
+      </table>
+      <br></br>
+      <div className='dip'>
+      <img src={myImage} alt='bbb' width={60}/>
+    </div>
+    </div>
+    <div>
+      <table className='sal'>
+        <tr>
+          <th> <span className='roi'> Orange</span> <br></br> <span className='qqq'>There are many variations of passages <br></br> of Lorem Ipsum available, but the <br></br> majority have suffered alteration in <br></br> some form, by injected humour, or <br></br> randomised words which don't look <br></br> even slightly believable. </span> </th>
+        </tr>
+      </table>
+      <br></br>
+      <div className='las'>
+      <img src={myImage} alt='bbb' width={60}/>
+    </div>
+    <div>
+      <table className='pid'>
+        <tr>
+          <th> <span className='roi'> Orange</span> <br></br> <span className='qqq'>There are many variations of passages <br></br> of Lorem Ipsum available, but the <br></br> majority have suffered alteration in <br></br> some form, by injected humour, or <br></br> randomised words which don't look <br></br> even slightly believable. </span> </th>
+        </tr>
+      </table>
+    </div>
+    </div>
+    <button className='lopp'> <span className='xip'>Read More</span> </button>
+    <br></br>
+    <h1 className='mine'>Fresh Fruits</h1>
+    <p className='enim'>There are many variations of passages of Lorem Ipsum available, but the majority have</p>
+    <br></br>
+    <div>
+    <h1 className='hap'>Best Fresh Orange</h1>
+    <p className='cap'>but the majority have suffered alteration in some form, by injected humour, or randomised <br></br> words which don't look even slightly believable. If you are going to use a passage of Lorem <br></br> Ipsum, you need to be</p>
+    <button className='ppol'>Buy Now</button>
+    <div className='game'>
+        <img src={myImages} alt='bbb' width={300}/>
+        <br></br>
+        <h1 className='pah'>Best Fresh Grapes</h1>
+       <p className='pac'>but the majority have suffered alteration in some form, by injected humour, or randomised <br></br> words which don't look even slightly believable. If you are going to use a passage of Lorem <br></br> Ipsum, you need to be</p>
+       <button className='chill'>Buy Now</button>
+        <div className='gam'>
+        <img src={myImagess} alt='bbb' width={150}/>
+        </div>
+        <br></br>
+        <div>
+          <h1 className='child'>Best Fresh Gauva</h1>
+          <p className='paren'>but the majority have suffered alteration in some form, by injected humour, or randomised <br></br> words which don't look even slightly believable. If you are going to use a passage of Lorem <br></br> Ipsum, you need to be</p>
+          <button className='chi'>Buy Now</button>
+          <div className='yam'>
+        <img src={myImagel} alt='bbb' width={250}/>
+        </div>
+        </div>
+        </div>
+    </div>
+    <br></br>
+    <div className='may'>
+        <img src={myPic} alt='bbb' width={1800} height={550}/>
+        </div>
+        <br></br>
+        <p className='poll'>Very tasty fruits</p>
+        <br></br>
+        <p className='llop'>Testimonial</p>
+        <p className='opl'>There are many variations of passages of Lorem Ipsum available, but the majority have</p>
+        <br></br>
+        <div id="indicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="0" className="active"></button>
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="1"></button>
+        </div>
+    <div class="container8">
+ <div class="bg-danger navbar-danger">
+  <div class="carousel-inner">
+  <div class="carousel-item active">
+    <p className='container10'>Johnhex</p>
+    <p className='eep'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,<br></br><span className='sss'> or randomised words which don't look even slightly believable. If you are </span><br></br><span className='sas'> going to use a passage of Lorem Ipsum, you need to be sure</span></p>
+    <br></br>
+    <div className='pict'>
+    <img src={myPicture} alt='bbb' width={400}/>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#indicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      </button>
+    
+      <button class="carousel-control-next" type="button" data-bs-target="#indicators" data-bs-slide="next">
+       <span class="carousel-control-next-icon"></span>
+      </button>
+  </div>
+  </div>
+  <div id="indicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+    <button type="button" data-bs-target="#indicators" data-bs-slide-to="0" class='active'></button>
+    <button type="button" data-bs-target="#indicators" data-bs-slide-to="1"></button>
+  </div>
+  <div class="carousel-inner">
+  <div class="carousel-item">
+  <div class="container9">
+  <p className='container10'>Johnhex</p>
+  <p className='eep'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,<br></br><span className='sss'> or randomised words which don't look even slightly believable. If you are </span><br></br><span className='sas'> going to use a passage of Lorem Ipsum, you need to be sure</span></p>
+  <br></br>
+  <div className='pict'>
+  <img src={myPicture} alt='bbb' width={400}/>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#indicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </button>
+  
+      <button class="carousel-control-next" type="button" data-bs-target="#indicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </button>
+        </div>
+  </div> 
+    </div>  
+    </div>
+    </div>
+    </div>
+    </div>
+    <p className='us'>Contact Us</p>
+    <form>
+    <input type="text" id="name" name="name" placeholder='Name' /> <br></br>
+    <input type="tel" id="phone" name="phone" placeholder='Phone Number' /> <br></br>
+    <input type="email" id="email" name="email" placeholder='Email' /> <br></br>
+    <span className='rep'> <input type="text" id="message" name="message" placeholder='Messages' /></span>
+    <br></br>
+    <button className='sub'> <span className='do'>Send</span> </button>
+  </form>
+     <p className='qip'>Fruits</p>
+     <p className='piq'>randomised <br></br>words which <br></br>don't look even <br></br>slightly <br></br>believable. If you <br></br>
+     are going to use <br></br>a passage of <br></br> Lorem Ipsum, </p>
+     <br></br>
+     <p className='ipq'>Services</p>
+     <p className='pqi'>randomised <br></br>words which <br></br>don't look even <br></br>slightly <br></br>believable. If you <br></br>
+     are going to use <br></br>a passage of <br></br> Lorem Ipsum, </p>
+     <br></br>
+     <p className='pwi'>List</p>
+     <p className='pio'>randomised <br></br>words which <br></br>don't look even <br></br>slightly <br></br>believable. If you <br></br>
+     are going to use <br></br>a passage of <br></br> Lorem Ipsum, </p>
+     <br></br>
+     <div>
+     <p className='rrp'>Follow Us</p>
+     <br></br>
+    <span className='loppi'><i class="fa-brands fa-facebook"></i>
+     <i class="fa-brands fa-twitter"></i>
+     <i class="fa-brands fa-linkedin"></i>
+     <i class="fa-brands fa-instagram"></i> </span> 
+     </div>
+     <div>
+      <form>
+      <span className='wlk'> <label for="fname">Subcribe Now</label></span><br></br>
+      < button  className="lll" type="text" id="fname" name="fname" />
+      </form>
+     </div>
+     <br></br>
+     <button className='mol'> <span className='rtl'> Subscribe</span></button>
+     <br></br>
+     <div class="alert alert-secondary"></div>
+     <br></br>
+     <p className='diol'>Copyright © 2019 All Rights Reserved By Free Html Templates</p>
+    </div>
+  )
+}
+
+export default Ass
